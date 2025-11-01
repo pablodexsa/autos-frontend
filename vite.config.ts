192be0ed99384157aad7a7694428c475e046b551
Pablo Fassari <pfassari@DTT.COM>
2025-10-31 18:29:59 -0700

    Actualizaci≤n

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      process.env.VITE_API_URL || "https://autos-backend-h3eb.onrender.com/api"
+    ),
+  },
+}));
